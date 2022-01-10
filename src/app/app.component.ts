35b83d29cb0c3f8005084d7539eb2f60f0502899
liuweihua <liuweihua@ihuayue.cn>
2022-01-10 23:37:35 +0800

    feat: partial i18n text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  p1 = `<element i18n="{i18n_metadata}">{string_to_translate}</element>`;
+  p2 = `<element i18n-{attribute_name}="{i18n_metadata}" {attribute_name}="{attribute_value}" />`;
+  p3 = `$localize \`:{i18n_metadata}:string_to_translate\``
+
+  number = 1234;
+  str = $localize`组件中的字符串`;
 }
